c13055afba399d5d9b108546056e8d7830e307ca
y-ken <y.ken.studio@gmail.com>
2013-07-19 00:10:17 +0900

    use Fluent::BuffertOutput instead of Fluent::Output
    It's performance planning which not to block event loop.

--- lib/fluent/plugin/out_geoip.rb
+++ lib/fluent/plugin/out_geoip.rb
@@ -1,4 +1,4 @@
-class Fluent::GeoipOutput < Fluent::Output
+class Fluent::GeoipOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('geoip', self)
   
   def initialize
@@ -8,17 +8,37 @@ class Fluent::GeoipOutput < Fluent::Output
 
   config_param :geoip_database, :string, :default => 'data/GeoLiteCity.dat'
 
+  include Fluent::HandleTagNameMixin
+  include Fluent::SetTagKeyMixin
+  config_set_default :include_tag_key, false
+
   def configure(conf)
     super
-
-    @geoip = GeoIP::City.new(@geoip_database)
+    
+    if ( !@remove_tag_prefix && !@remove_tag_suffix && !@add_tag_prefix && !@add_tag_suffix )
+      raise ConfigError, "geoip: Set remove_tag_prefix, remove_tag_suffix, add_tag_prefix or add_tag_suffix."
+    end
+    
+    @geoip = GeoIP::City.new(@geoip_database, :memory, false)
   end
 
-  def emit(tag, es, chain)
-    es.each do |time,record|
-      $log.info "geoip: #{record['host']} : #{@geoip.look_up(record['host'])}"
-    end
+  def start
+    super
+  end
+  
+  def format(tag, time, record)
+    [tag, time, record].to_msgpack
+  end
+  
+  def shutdown
+    super
+  end
 
-    chain.next
+  def write(chunk)
+    chunk.msgpack_each do |tag, time, record|
+      result = @geoip.look_up(record['host'])
+      $log.info "geoip: #{record['host']} : #{result}"
+      Fluent::Engine.emit(tag, time, result)
+    end
   end
 end
