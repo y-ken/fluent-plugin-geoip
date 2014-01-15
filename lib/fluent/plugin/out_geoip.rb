f8901aee55ff4a9ae8b05f315e9f401511187a0d
muddydixon <muddydixon@gmail.com>
2014-01-15 21:53:21 +0900

    enable to use nested ip in record

--- lib/fluent/plugin/out_geoip.rb
+++ lib/fluent/plugin/out_geoip.rb
@@ -1,6 +1,6 @@
 class Fluent::GeoipOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('geoip', self)
-  
+
   GEOIP_KEYS = %w(city latitude longitude country_code3 country_code country_name dma_code area_code region)
   config_param :geoip_database, :string, :default => File.dirname(__FILE__) + '/../../../data/GeoLiteCity.dat'
   config_param :geoip_lookup_key, :string, :default => 'host'
@@ -13,7 +13,7 @@ class Fluent::GeoipOutput < Fluent::BufferedOutput
     require 'geoip'
     super
   end
-  
+
   def configure(conf)
     super
 
@@ -23,6 +23,9 @@ class Fluent::GeoipOutput < Fluent::BufferedOutput
       raise Fluent::ConfigError, "geoip: unsupported key #{geoip_key_name}" unless GEOIP_KEYS.include?(geoip_key_name)
       @geoip_keys_map.store(geoip_key_name, conf[key])
     end
+
+    @geoip_lookup_key = @geoip_lookup_key.split(".")
+
 
     if ( !@remove_tag_prefix && !@remove_tag_suffix && !@add_tag_prefix && !@add_tag_suffix )
       raise Fluent::ConfigError, "geoip: missing remove_tag_prefix, remove_tag_suffix, add_tag_prefix or add_tag_suffix."
@@ -34,11 +37,11 @@ class Fluent::GeoipOutput < Fluent::BufferedOutput
   def start
     super
   end
-  
+
   def format(tag, time, record)
     [tag, time, record].to_msgpack
   end
-  
+
   def shutdown
     super
   end
@@ -49,8 +52,17 @@ class Fluent::GeoipOutput < Fluent::BufferedOutput
     end
   end
 
+  def get_address(record)
+    obj = record
+    @geoip_lookup_key.each do |key|
+      return nil if not obj.has_key?(key)
+      obj = obj[key]
+    end
+    obj
+  end
+
   def add_geoip_field(record)
-    address = record[@geoip_lookup_key]
+    address = get_address(record)
     return record if address.nil?
     result = @geoip.look_up(address)
     return record if result.nil?
