d5f97ee2c90b9a2f693de2d309776070748b59ac
Kenji Okimoto <okimoto@clear-code.com>
2015-08-05 08:12:49 +0900

    Add missing spaces between arguments

--- test/plugin/test_filter_geoip.rb
+++ test/plugin/test_filter_geoip.rb
@@ -12,7 +12,7 @@ class GeoipFilterTest < Test::Unit::TestCase
     tag               geoip.${tag}
   ]
 
-  def create_driver(conf=CONFIG,tag='test',use_v1=false)
+  def create_driver(conf=CONFIG, tag='test', use_v1=false)
     Fluent::Test::FilterTestDriver.new(Fluent::GeoipFilter, tag).configure(conf, use_v1)
   end
 
